7db24024f5461c1b3f1ba5abb061b9eea3191bcd
ilia24 <iliademertchian24@gmail.com>
2017-04-20 15:20:28 -0400

    Create model for Resturant class

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420182558) do
+ActiveRecord::Schema.define(version: 20170420191847) do
+
+  create_table "resturants", force: :cascade do |t|
+    t.string   "name"
+    t.time     "opentime"
+    t.time     "closetime"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
