88ac7e947bcbdebe7a0f90e1277a8ae355166d0b
pritam-das <pritam.das@mail.utoronto.ca>
2017-04-21 13:31:00 -0400

    Adding option to add image into new restaurant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421160649) do
+ActiveRecord::Schema.define(version: 20170421171143) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "user_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170421160649) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "availability"
+    t.string   "image_url"
   end
 
   create_table "reviews", force: :cascade do |t|
