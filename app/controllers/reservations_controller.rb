125e50fe457f371e7e64bcb6a6d25b868efd71a8
codonculous <feidameng01@gmail.com>
2017-04-21 12:10:58 -0400

    create load_reservation method

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,57 @@
 class ReservationsController < ApplicationController
+  before_action :load_reservation
+  before_action :ensure_logged_in, only: [:new, :create, :destroy, :edit, :destroy, :update]
+
+  def show
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = @restaurant.reservation.build(reservation_params)
+    @reservation.user = current_user
+    if @reservation.save
+      redirect_to restaurant_path, notice: 'Reservation booked successfully'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def updated
+    @reservation = Reservation.find(params[:id])
+    if @reservation.update_attributes(reservation_params)
+      redirect_to restaurant_reservation
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    @reservation.destroy
+    redirect_to restaurant_path
+
+  end
+
+
+
+  private
+  def reservation_params
+    params.require(:reservation).permit(:user_id, :restaurant_id, :time, :date)
+  end
+
+  def load_reservation
+   @product = Rerservation.find(params[:Reservation_id])
+  end
+
+
+
 end
