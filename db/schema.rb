fcfa9e3eb3ebe55ae1b56b4aeab16691d4487985
ilia24 <iliademertchian24@gmail.com>
2017-04-21 19:33:21 -0400

    Add Timeslot model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421171143) do
+ActiveRecord::Schema.define(version: 20170421233209) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "user_id"
@@ -37,6 +37,15 @@ ActiveRecord::Schema.define(version: 20170421171143) do
     t.integer  "user_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "timeslots", force: :cascade do |t|
+    t.date     "start"
+    t.date     "end"
+    t.integer  "cap"
+    t.integer  "people"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
