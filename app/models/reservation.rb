583cbd8fa3959bdb8ef12a49e61f1008b4445e28
codonculous <feidameng01@gmail.com>
2017-04-23 17:51:26 -0400

    debug assocaition

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,7 +2,7 @@ class Reservation < ApplicationRecord
   belongs_to :restaurant
   belongs_to :user
   has_many :reserves
-  has_many :timeslots, through: :reserves
+  has_and_belongs_to_many :timeslots
 
 
 
