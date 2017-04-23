578c17775314df761f7c09ea288228fb15416490
codonculous <feidameng01@gmail.com>
2017-04-23 15:50:40 -0400

    debug for views

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,7 +1,7 @@
 class ReservationsController < ApplicationController
 
   before_action :load_restaurant
-  before_action :ensure_logged_in, only: [:new, :create, :destroy, :edit, :destroy, :update]
+  before_action :ensure_logged_in, only: [:new, :create, :edit, :destroy, :update]
 
   def show
     @reservation = Reservation.find(params[:id])
