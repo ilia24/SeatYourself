149fa13fee4f95635e1e82baf7f5506debf82025
ilia24 <iliademertchian24@gmail.com>
2017-04-22 23:43:00 -0400

    Add base reservation function

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,10 @@ class Reservation < ApplicationRecord
   belongs_to :restaurant
   belongs_to :user
   has_and_belongs_to_many :timeslots
+
+  def self.placeholder(starttime, endtime, peep)
+    slots = Timeslot.where("start >= ? AND end <= ?", starttime, endtime).update_all(people: peep)
+    return slots
+  end
+
 end
